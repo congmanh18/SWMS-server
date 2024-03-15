b67c77c671c348a0b3b8d3a4b0a43e244d5e682d
congmanh18 <nguyenmanh1801@gmail.com>
2024-03-15 17:13:47 +0700

    [Update] merge repo > handlers

--- internal/app/handlers/user_handler.go
+++ internal/app/handlers/user_handler.go
@@ -1,65 +1,69 @@
 package handlers
 
 import (
-	"fmt"
 	"net/http"
 	"smart-waste-system/internal/app/models"
 	"smart-waste-system/internal/utils"
+	"time"
 
+	"github.com/go-playground/validator"
 	"github.com/gofiber/fiber/v2"
 	"github.com/google/uuid"
 )
-
-func (uh *Repository) Muoba(context *fiber.Ctx) error {
-	user := models.User{}
-	user.ID = uuid.New().String()
-
-	err := context.BodyParser(&user)
-	fmt.Println(user)
-	if err != nil {
-		return utils.HandleErrorResponse(context, http.StatusUnprocessableEntity, "Request failed")
-	}
-	if err := uh.CreateAccount(context, user); err != nil {
-		return utils.HandleErrorResponse(context, http.StatusInternalServerError, "Failed to register user")
-	}
-
-	return context.Status(http.StatusOK).JSON(&fiber.Map{
-		"message": "user has been added"})
-}
 
 func (uh *Repository) Register(ctx *fiber.Ctx) error {
 	user := models.User{}
 	user.ID = uuid.New().String()
 
 	err := ctx.BodyParser(&user)
-	fmt.Println(user)
 	if err != nil {
 		return utils.HandleErrorResponse(ctx, http.StatusUnprocessableEntity, "Invalid request payload")
 	}
+	validationErr := validator.New().Struct(user)
+	if validationErr != nil {
+		return utils.HandleErrorResponse(ctx, http.StatusBadRequest, "Validation failed")
+	}
 
-	// Gọi hàm đăng ký user từ Repository
-	if err := uh.CreateAccount(ctx, user); err != nil {
-		return utils.HandleErrorResponse(ctx, http.StatusInternalServerError, "Failed to register user")
+	token, _ := utils.GenerateAllTokens(user.Phone, user.ID, *user.FirstName, *user.MiddleName, *user.LastName)
+	user.Token = &token
+
+	password := utils.HashPassword(*user.Password)
+	user.Password = &password
+
+	user.CreatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+	user.UpdatedAt, _ = time.Parse(time.RFC3339, time.Now().Format(time.RFC3339))
+
+	err = uh.DB.Create(&user).Error
+	if err != nil {
+		return utils.HandleErrorResponse(ctx, http.StatusBadRequest, "Could not create user")
 	}
 
 	// Trả về thành công nếu không có lỗi
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
+		"info":    user,
 		"message": "register successfully"})
 }
 
 func (uh *Repository) Login(ctx *fiber.Ctx) error {
 	user := models.User{}
+	foundUser := models.User{}
 
 	err := ctx.BodyParser(&user)
-	// fmt.Println(err)
 	if err != nil {
 		return utils.HandleErrorResponse(ctx, http.StatusUnprocessableEntity, "Invalid request payload")
 	}
-
-	foundUser, err := uh.SigninByPhoneNumber(ctx, user)
-	if err != nil {
-		return utils.HandleErrorResponse(ctx, http.StatusBadRequest, "Request Login failed")
+	if err := uh.DB.Where("id = ?", user.ID).First(&foundUser).Error; err != nil {
+		return utils.HandleErrorResponse(ctx, http.StatusUnprocessableEntity, err.Error())
 	}
+
+	passwordIsValid, msg := utils.VerifyPassword(*user.Password, *foundUser.Password)
+	if !passwordIsValid {
+		return utils.HandleErrorResponse(ctx, http.StatusInternalServerError, msg)
+	}
+
+	token, refreshToken := utils.GenerateAllTokens(foundUser.Phone, foundUser.ID, *foundUser.FirstName, *foundUser.MiddleName, *foundUser.LastName)
+	utils.UpdateAllTokens(uh.DB, foundUser.ID, token, refreshToken)
+
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message":    "login successfully",
 		"found user": foundUser})
@@ -67,10 +71,14 @@ func (uh *Repository) Login(ctx *fiber.Ctx) error {
 
 func (uh *Repository) ViewInfo(ctx *fiber.Ctx) error {
 	userID := ctx.Params("id")
+	user := &models.User{}
+	if err := utils.MatchUserTypeToUID(ctx, userID); err != nil {
+		return utils.HandleErrorResponse(ctx, http.StatusBadRequest, "Request Get failed")
+	}
 
-	user, err := uh.GetUserByID(ctx, userID)
+	err := uh.DB.Where("id = ?", userID).First(user).Error
 	if err != nil {
-		return utils.HandleErrorResponse(ctx, http.StatusInternalServerError, "Failed to get user by ID")
+		return utils.HandleErrorResponse(ctx, http.StatusInternalServerError, "user not found")
 	}
 	return ctx.Status(http.StatusOK).JSON(&fiber.Map{
 		"message": "information",
